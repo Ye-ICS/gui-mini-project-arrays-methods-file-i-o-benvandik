8f0b8e680dd1f27038433500bbbe4bfd1750cedb
benvandik <chahineomer7@gmail.com>
2025-12-01 12:09:56 -0500

    basic gui

--- App.java
+++ App.java
@@ -1,66 +1,65 @@
+import java.io.File;
+
 import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- * Template JavaFX application.
- */
+
 public class App extends Application {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         launch(args);
     }
-    
+
     @Override
-    public void start(Stage stage) {
-        // Create components to add.
-        VBox contentBox = new VBox();
-        contentBox.setAlignment(Pos.CENTER);
+    public void start (Stage stage){
 
-        Label promptLabel = new Label();
-        promptLabel.setText("Enter your thoughts");
+        VBox mainBox = new VBox();
+            mainBox.setStyle ("-fx-background-color: darkslateblue; -fx-padding: 20;");
+            mainBox.setSpacing(15);
 
-        TextField thoughtsBox = new TextField();
-        thoughtsBox.setMaxWidth(150);
-        thoughtsBox.setPromptText("type here");
-        
-        TextArea messageBox = new TextArea();
-        messageBox.setEditable(false);
+            HBox containerBox = new HBox();
+                containerBox.setStyle("-fx-background-color: dimgray; -fx-border-color: limegreen; -fx-border-width: 2; -fx-padding: 10;");
+                containerBox.setSpacing(15);
 
-        Button submissionBtn = new Button();
-        submissionBtn.setText("Submit");
 
-        // Set up reactions (aka callbacks).
-        submissionBtn.setOnAction(event -> onSubmitThought(thoughtsBox, messageBox));
+            Label instructionsLBL = new Label();
+                instructionsLBL.setText("Enter as much grades as you want and also write on how much they are 😉 ");
+                instructionsLBL.setStyle ("-fx-font-size: 16; -fx-text-fill: lightgray;");
 
-        // Add components to the content box.
-        contentBox.getChildren().add(promptLabel);
-        contentBox.getChildren().add(thoughtsBox);
-        contentBox.getChildren().add(submissionBtn);
-        contentBox.getChildren().add(messageBox);
+            TextField gradeBox = new TextField();
+                gradeBox.setMaxWidth(150);
+                gradeBox.setPromptText("Example: 54,34...");
+                gradeBox.setStyle("-fx-background-color: darkgray; -fx-text-fill: white; -fx-prompt-text-fill: gray; -fx-padding: 8;");
 
-        // Set up the window and display it.
-        Scene scene = new Scene(contentBox, 300, 200);
+                TextField ovrBox = new TextField();
+                ovrBox.setMaxWidth(150);
+                ovrBox.setPromptText("Example: 100 or 50 or 20");
+                ovrBox.setStyle("-fx-background-color: darkgray; -fx-text-fill: white; -fx-prompt-text-fill: gray; -fx-padding: 8;");
+                
+            Button sendBtn = new Button ("Send 🤞");
+                sendBtn.setStyle ("-fx-background-color: limegreen; -fx-text-fill: black; -fx-font-size: 15px; -fx-font-weight: bold; -fx-padding: 10;");
+
+                Label resultLBL = new Label();
+                resultLBL.setStyle("-fx-text-fill: white; -fx-font-size: 14; -fx-padding: 8;");
+                mainBox.getChildren().add(resultLBL);
+
+                // set actions.
+                sendBtn.setOnAction(event -> {
+                    String[] gradeStrings = gradeBox.getText().split(" ");
+                    Double[] grade = new Double[gradeStrings.length];
+
+                });
+
+        mainBox.getChildren().addAll(instructionsLBL,gradeBox, ovrBox, sendBtn);
+
+        Scene scene = new Scene (mainBox, 700, 600);
         stage.setScene(scene);
-        stage.setTitle("Amazing App 2000");
+        stage.setTitle("My Final Grades 😎😁👌");
         stage.show();
-    }
-
-    /**
-     * Handle the submission of a thought.
-     * @param inputBox  The TextField where the user types their thought.
-     * @param outputBox The TextArea where the submitted thoughts are displayed.
-     */
-    void onSubmitThought(TextField inputBox, TextArea outputBox) {
-        String text = inputBox.getText();
-        inputBox.clear();
-        System.out.println("Interesting thought: " + text);
-        outputBox.appendText("Interesting thought: " + text + "\n");
     }
 }
